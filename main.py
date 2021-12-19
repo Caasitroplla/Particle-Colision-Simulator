16f4050bbcac342cd1bfe572252d254e81eda1c9
Isaac <caasitroplla@gmail.com>
2021-12-19 13:52:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-# This is where the collision simulator is ran
-print("Testing testing 123")
+from src.particle import Particle
+from src.vector import Vector
+from src.scene import Scene
+from src.collision import collide
+from json import load
+
+def main():
+	# load json
+	with open("particles.json", "r") as file:
+		data = load(file)
+		print(data)
+	
+	# make particles from json
+	particles = []
+	for item in data["particles"]:
+		newParticle = Particle(item["x"], item["y"], item["x_velos"], item["y_velos"], item["mass"])
+		particles.append(newParticle)
+	
+	# load scene
+	scene = Scene(particles)	
+
+if __name__ == '__main__':
+	main()
